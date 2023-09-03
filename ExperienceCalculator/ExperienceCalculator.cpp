a94298517bbaeaa9a24dab7af3dec3e5f5f7c3a2
Andrei Gridin <andreigridin918@gmail.com>
2023-09-03 22:09:53 +0500

    Добавьте файлы проекта.

---
+++ ExperienceCalculator/ExperienceCalculator.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    // Изменяем кодировку консоли:
+    system("chcp 1251 > nul");
+
+    int level1 = 1000;
+    int level2 = 2500;
+    int level3 = 5000;
+    int points = 0;
+
+    cout << "Введите кол-во очков: ";
+    cin >> points;
+    cout << "-------Проверяем-------" << endl;
+
+    if (points >= level3) {
+        cout << "Ваш уровень: 3" << endl;
+    }
+    else {
+        if (points >= level2) {
+            cout << "Ваш уровень: 2" << endl;
+        }
+        else {
+            if (points >= level1) {
+                cout << "Ваш уровень: 1" << endl;
+            }
+            else {
+                if (points < 0) {
+                    cout << "Не корректно введено значение!" << endl;
+                }
+                else {
+                    if (points < level1) {
+                        cout << "Вы всё ещё на нулевом уровне!" << endl;
+                    }
+                }
+            }
+        }
+    }
+
+    // Задержка окна консоли:
+    system("pause > nul");
+    return 0;
+}
